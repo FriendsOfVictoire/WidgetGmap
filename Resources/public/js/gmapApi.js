529a52a97b0211f288659a2fca565891c12e4439
Charlie <charlie@appventus.com>
2015-10-14 17:43:23 +0200

    add a js to call the gmaps api once

---
+++ Resources/public/js/gmapApi.js
@@ -0,0 +1,47 @@
+//this is just an example to call multiple apis in a future with some config
+
+//the problem is that google maps api correct callback must be send in request
+var apis = {
+    'gmap': 'https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=places&callback=triggerGmapEvent'
+};
+
+// list of apis that are loaded
+var loadedApis = [];
+
+//we load apis
+window.onload = loadApis(apis);
+
+function loadApis(apis)
+{
+    for(var apiName in apis){
+        if((loadedApis.indexOf(apiName)))
+        {
+            //we add script to load them
+            //TODO send a callback that work correctly when api is fully load
+
+            addScript(apis[apiName]);
+            loadedApis.push(apiName);
+        }
+    };
+}
+
+//a general callback to trigger an api event
+function triggerApiEvent(apiName)
+{
+    $(document).trigger(apiName);
+}
+
+//use a an event specific for gmap callback
+function triggerGmapEvent()
+{
+    $(document).trigger('gmap');
+}
+
+// this function add the script and allow to use a callback(doesn't work for Gmap Api)
+function addScript( url, callback ) {
+    var script = document.createElement( 'script' );
+    if( callback ) script.onload = callback;
+    script.type = 'text/javascript';
+    script.src = url;
+    document.head.appendChild( script );
+}
